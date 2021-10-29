4873108cf9dbbe49a09b7ef5f5ce832ded668ac9
mattcoosh <mcuschie@risd.edu>
2021-10-29 11:01:43 -0400

    Update my.js

--- my.js
+++ my.js
@@ -2,6 +2,7 @@ var S1s = ['giraffe', 'turtle', 'human'];
 var S2s = ['blue', 'silly', 'deeply wise'];
 var bSOnes = ['monkey', 'fruit', 'banana'];
 var bsTwos = ['blue', 'silly', 'deeply wise'];
+var images = ['image/ME-4024-Sports-Ball-Soccer-2__42273.webp','','','','','','','','',]
 
 function make_random_sentence()
 {
@@ -9,14 +10,17 @@ function make_random_sentence()
   let S2 = S2s[Math.floor(Math.random() * S2s.length)];
   let bSOne = bSOnes[Math.floor(Math.random() * bSOnes.length)];
   let bsTwo = bsTwos[Math.floor(Math.random() * bsTwos.length)];
+  let image = images[Math.floor(Math.random() * images.length)];
 
   let S1_slot = $('#S1-slot');
   let S2_slot = $('#S2-slot');
-  let bSOne_slot = $('#bSOne_slot');
-  let bsTwo_slot = $('#bsTwo_slot');
+  let bSOne_slot = $('#bSOne-slot');
+  let bsTwo_slot = $('#bsTwo-slot');
+  let image_slot = $('#image-slot');
 
   S1_slot.text(S1);
   S2_slot.text(S2);
+  image_slot.text(image);
   bSOne_slot.text(bSOne);
   bsTwo_slot.text(bsTwo);
 }
